a260823f7d876c02a0d088d9522419e2e1ab5339
Václav Tregner <vtregner@csas.cz>
2023-06-12 14:49:37 +0200

    fix: benefits mock server

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Req } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { ExternalUserIdRequest } from "./web-api/api/events/external-user-id-request";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): any {
     return this.appService.getData();
+  }
+
+  @Post('/webapi/api/v1/events/externalIdentifiers')
+  getExternalUserIdV1(@Req() _req: ExternalUserIdRequest) {
+    return this.appService.getExternalUserId();
+  }
+
+  @Post('/webapi/api/v2/events/externalIdentifiers')
+  getExternalUserIdV2(@Req() _req: ExternalUserIdRequest) {
+    return this.appService.getExternalUserId();
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,6 @@
-import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
-import { INestApplication } from '@nestjs/common';
+import { Test } from "@nestjs/testing";
+import { AppModule } from "./../src/app.module";
+import { INestApplication } from "@nestjs/common";
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -13,12 +12,5 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
   });
 });
